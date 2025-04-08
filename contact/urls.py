eceae751486fd37ffb308521a5183cd311488a70
Пользователь <iskandarovfirdavs@gmail.com>
2025-04-08 18:01:23 +0500

    smt

--- contact/urls.py
+++ contact/urls.py
@@ -5,4 +5,5 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', ContactView.as_view(), name='contact'),
+    
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import ContactForm  
 
-
-class ContactView(TemplateView):
+class ContactView(View):
     template_name = 'contact.html'
+
+    def get(self, request):
+        form = ContactForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()  
+            return redirect("contact:contact")  
+        return render(request, self.template_name, {'form': form})
